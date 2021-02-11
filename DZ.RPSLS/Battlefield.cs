40e68d988a6760371f7420acec6d7551fc18a2aa
DonaldZheng <dzheng112@gmail.com>
2021-02-11 11:24:56 -0600

    added classes

---
+++ DZ.RPSLS/Battlefield.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ.RPSLS
+{
+    class Battlefield
+    {   //Scissors cuts paper 
+        //Rock crushes Scissors  
+        //Scissors cuts Paper
+        //Paper covers Rock
+        //Rock crushes Lizard
+        //Lizard poisons Spock
+        //Spock smashes Scissors
+        //Scissors decapitates Lizard
+        //Lizard eats Paper
+        //Paper disproves Spock
+        //Spock vaporizes Rock
+
+
+        //member variables (HAS A)
+
+        //constructor
+        public Battlefield()
+        {
+
+        }
+
+        //member methods (CAN DO)
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to RPSLS!");
+        }
+
+        public void DisplayRules()
+        {
+            Console.WriteLine("You will choose Rock, Paper, Scissors, Lizard, or Spock. " +
+                "First to 3 Points will be declared the winner. Each rounds is worth one point");
+        }
+
+
+
+
+
+    }
+}
---
+++ DZ.RPSLS/Paper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ.RPSLS
+{
+    class Paper
+    {
+        //member variables (HAS A)
+
+        //constructor
+
+        //member methods (CAN DO)
+    }
+}
